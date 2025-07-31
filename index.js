e6e4562e2686cc7f1acb12d7c07a612e97812610
wdsaviya <128067789+wdsaviya@users.noreply.github.com>
2025-08-01 02:39:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "This is an API response" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
